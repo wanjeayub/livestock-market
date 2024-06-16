4c5a624530738e786476836aba7fefaa0a8e8e06
Ayub Wanje <ayub.wanje@gmail.com>
2024-06-17 00:37:01 +0300

    updated product details page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/images/lmarket-logo.png";
 
 export const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between p-4 px-7 mx-auto shadow-md mb-4 ">
+    <header className="flex flex-row items-center justify-between p-4 px-7 mx-auto shadow-md mb-4 bg-slate-100 ">
       <div className="main ">
         <Link to={"/"}>
           <div className="logo">
--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -3,40 +3,80 @@ import mainGoat from "../assets/images/licensed-image.jpg";
 
 export const ProductDetails = () => {
   return (
-    <div className="flex flex-row">
-      <section className="px-7">
-        <div className="flex flex-row">
+    <div className="flex flex-col px-7">
+      <div className="flex flex-col md:flex-row gap-4">
+        <section className="p-7 bg-slate-100 w-fit rounded-md">
+          <div className="flex flex-col md:flex-row shadow-sm">
+            <div>
+              <img
+                className=" md:h-[300px]"
+                src={mainGoat}
+                alt="listing details image"
+              />
+            </div>
+            <div className="flex flex-col md:px-4 gap-2">
+              <p className="pt-4 text-2xl md:text-3xl">Name: Mountain Goat</p>
+              <p className="text-xl">Breed: Capra Grigia</p>
+              <p className="text-xl">Weight:10Kg</p>
+              <p className="text-2xl md:text-3xl font-semibold">Ksh 7,000</p>
+              <p className="text-xl">
+                Seller:
+                <Link to={"/shop"}>Wanje | similar listings from Wanje</Link>
+              </p>
+              <p className="text-sm text-slate-600">In stock</p>
+              <p className="text-sm text-slate-600">
+                + Shipping from Ksh 2,000 to Nairobi
+              </p>
+              <button className="bg-lime-500 p-3 rounded-md w-full text-white">
+                Add to Cart
+              </button>
+            </div>
+          </div>
           <div>
-            <img
-              className="h-[300px]"
-              src={mainGoat}
-              alt="listing details image"
-            />
+            <p>Add Promo stuff</p>
           </div>
-          <div className="flex flex-col px-4 gap-3">
-            <p className="text-3xl">Name: Mountain Goat</p>
-            <p className="text-xl">Breed: Capra Grigia</p>
-            <p className="text-xl">Weight:10Kg</p>
-            <p className="text-3xl font-semibold">Ksh 7,000</p>
-            <p className="text-xl">
-              Seller:
-              <Link to={"/shop"}>Wanje | similar listings from Wanje</Link>
+        </section>
+        <aside>
+          <div>
+            <h1>Delivery & Returns</h1>
+          </div>
+        </aside>
+      </div>
+
+      <section className="px-7 p-3 flex flex-row gap-5">
+        <div className="">
+          <div className="b border-b-2 pb-2">
+            <h1 className="text-2xl">Product Details</h1>
+          </div>
+          <div className="text pt-5">
+            <p className="p-2">
+              Immerse yourself in stunning visuals with our sleek and compact
+              WK-32&quot;TV. Perfect for small to medium-sized spaces, this TV
+              delivers a crisp and vibrant viewing experience that brings your
+              favorite shows and movies to life. It carries a powerful
+              multi-surround sound effect that increases the excitement and
+              takes your normal sports watching a pleasurable experience. WK
+              32&quot TV comes with Smart features like a dedicated web browser
+              and popular apps like YouTube and a range of movie on demands
+              services. It is frameless and does not support Bluetooth.
             </p>
-            <p className="text-sm text-slate-600">In stock</p>
-            <p className="text-sm text-slate-600">
-              + Shipping from Ksh 2,000 to Nairobi
+            <p className="p-2">
+              Technical Specifications Display Screen size: 32&quot; Resolution:
+              1366*768 Tv Technology: SMART LED Connectivity Ports: USB(2), HDMI
+              (2), AV input(1) Cabinet Audio System Bright treble, full mid
+              range, deep bass, bring you and your family a thrilling listening
+              experience. DNR Noise Reduction Technology Built-in 3D noise
+              reduction make the signal stable and the picture realistic.
+              Additional Features HD - More Vivid Images No light dot, restore
+              natural colors, 360 degree reproduction of scene. Multi-interface,
+              multi-play, enjoy streaming experience.
             </p>
-            <button className="bg-lime-500 p-3 rounded-md w-full text-white">
-              Add to Cart
-            </button>
           </div>
         </div>
-        <br />
-        <div>
-          <p>Add Promo stuff</p>
-        </div>
+        <aside className="flex-none w-1/4">
+          <h1>Aside</h1>
+        </aside>
       </section>
-      <section></section>
     </div>
   );
 };
